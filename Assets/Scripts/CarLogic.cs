3fc006f3f004af279e8f3c65c09e4b77f65bb88c
Julio Quiroz <Julio Quiroz>
2017-06-03 10:33:47 -0400

    first commit

---
+++ Assets/Scripts/CarLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarLogic : MonoBehaviour {
+
+    Transform myTransform;
+    Rigidbody myRigidbody;
+    private void Awake()
+    {
+        myTransform = GetComponent<Transform>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        myRigidbody.AddForce(myTransform.forward*10);
+        
+    }
+}
